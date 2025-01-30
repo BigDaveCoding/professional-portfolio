763b65e924d810f25ff8a4c9875e5936d95630e0
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-30 20:42:30 +0000

    created function to emulate a text cursor which is placed after the hero_subtitle. gives impression of typing

--- script.js
+++ script.js
@@ -8,10 +8,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const nav_menu_small_screen = document.querySelector('.nav_menu_small_screen')
 
     const hero_subtitle = document.querySelector('.hero_subtitle')
+    const text_cursor = document.querySelector('.text_cursor')
 
     nav_menu_button.addEventListener('click', () => {
         nav_menu_small_screen.classList.toggle('hidden')
     })
+
+    function animateTextCursor(cursor) {
+
+        const tl = gsap.timeline({repeat: -1})
+
+        tl.to(cursor, {
+            opcacty: 1,
+            duration: 0.1
+        })
+        tl.to(cursor, {
+            duartion: 0.2
+        })
+        tl.to(cursor, {
+            opacity: 0,
+            duration: 0.3
+        })
+        tl.to(cursor, {
+            duration: 0.1
+        })
+        
+    }
 
     function animateSubtitle(){
         const prefix = 'I am a ';
@@ -19,7 +41,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         const icon = ['<i class="fa-solid fa-laptop-code"></i>', '<i class="fa-solid fa-brush"></i>', '<i class="fa-solid fa-code"></i>', '<i class="fa-solid fa-book"></i>', '<i class="fa-solid fa-toolbox"></i>', 
                         '<i class="fa-solid fa-gamepad"></i>', '<i class="fa-solid fa-lightbulb"></i>', '<i class="fa-solid fa-microchip"></i>']
         const typeSpeed = 0.15; // Typing speed in seconds per character
-        const delayBetweenWords = 3; // Delay in seconds between words
+        const delayBetweenWords = 1; // Delay in seconds between words
 
         const timeline = gsap.timeline({ repeat: -1, repeatDelay: delayBetweenWords }); //infinte loop with delay between words.
 
@@ -34,7 +56,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 ease: 'none'
             });
             timeline.to(hero_subtitle, {
-                text: '|',
+                text: '',
                 duration: word.length * (typeSpeed / 2),
                 ease: 'none'
             });
@@ -42,6 +64,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
 
     animateSubtitle()
+    animateTextCursor(text_cursor)
     
 });
 
