b1752b94dab7879f7626bd8b0e49404aadd23007
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-30 20:04:41 +0000

    installing gsap with cdn

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 "use strict";
 
-const nav_menu_button = document.querySelector('.nav_menu_button')
-const nav_menu_small_screen = document.querySelector('.nav_menu_small_screen')
 
-nav_menu_button.addEventListener('click', () => {
-    nav_menu_small_screen.classList.toggle('hidden')
-})
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger,TextPlugin)
+    // code here!
+    const nav_menu_button = document.querySelector('.nav_menu_button')
+    const nav_menu_small_screen = document.querySelector('.nav_menu_small_screen')
+
+    nav_menu_button.addEventListener('click', () => {
+        nav_menu_small_screen.classList.toggle('hidden')
+    })
+   });
+
+
 
